5edc734bf792abf0db22bdfd7ee32ec916e31690
Luisander2651 <luisander2651@gmail.com>
2025-03-20 17:55:17 -0600

    se configuro una nueva ruta para manejar el movimiento manual del carrito

--- services/websocketService.js
+++ services/websocketService.js
@@ -5,13 +5,13 @@ const modoManualWS = require('../routes/websockets/modomanualWS');
 function setupWebSocketServer(server) {
   // Crear servidor WebSocket
   const wssMovimientos = new WebSocket.Server({ server, path: '/movimientos' });
-  const wssModoManual = new WebSocket.Server({ server, path: '/modoManual' });
+  const wssModoManual = new WebSocket.Server({ server, path: '/modomanual' });
 
   // Manejar conexiones WebSocket
   wssMovimientos.on('connection', setupMovimientosWS);
   wssModoManual.on('connection', modoManualWS);
 
-  return {wssMovimientos, wssModoManual};
+  return wssMovimientos, wssModoManual;
 }
 
 module.exports = setupWebSocketServer;
